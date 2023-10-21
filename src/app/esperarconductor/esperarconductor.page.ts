76e603a5758e676da711537786b1bb60ab5db9aa
Juiceb0x21 <pavezzalex@gmail.com>
2023-10-20 23:35:51 -0300

    pag

--- src/app/esperarconductor/esperarconductor.page.ts
+++ src/app/esperarconductor/esperarconductor.page.ts
@@ -1,4 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, NgZone, OnInit } from '@angular/core';
+
+import {
+  FormGroup,
+  FormControl,
+  Validator,
+  FormBuilder,
+  Validators
+} from '@angular/forms'
+
+import { Router } from '@angular/router';
+import { MenuController, ToastController } from '@ionic/angular';
+
+import * as L from "leaflet";
+
+import { Geolocation } from '@capacitor/geolocation'
+import Swal from 'sweetalert2'
+import 'leaflet-routing-machine';
+
+import { ConductoresService } from '../services/api/conductores.service';
+
+
+
+
+
+declare var google: any;
+
 
 @Component({
   selector: 'app-esperarconductor',
@@ -6,10 +32,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./esperarconductor.page.scss'],
 })
 export class EsperarconductorPage implements OnInit {
+  latitud!: number;
+  longitud!: number;
+  private googleAutocomplete = new google.maps.places.AutocompleteService();
+  public searchResults = new Array<any>();
+  public destination: any;
+  private destino: L.Marker | null = null;
+  private locationMe!: L.Marker;
+  private control: any | null = null;
 
-  constructor() { }
+  conductor: any;
+  conductorUpdate: any;
+
+  public search: string = '';
+
+  map!: L.Map;
+
+  startPoint: any;
+  endPoint: any;
+
+
+  constructor(
+    private router: Router, 
+    private menu: MenuController, 
+    public fb: FormBuilder,
+    private toastController: ToastController,
+    private ngZone:  NgZone,
+    private conductoresService: ConductoresService,
+    private el: ElementRef,
+  ) { }
+
+  
+  async obtenerCoordenadas(){
+    const obtenerCoordenadas = await Geolocation.getCurrentPosition()
+    this.latitud = obtenerCoordenadas.coords.latitude;
+    this.longitud = obtenerCoordenadas.coords.longitude;
+    console.log(this.latitud)
+    console.log(this.longitud)
+  }
 
   ngOnInit() {
+    this.conductor = JSON.parse(localStorage.getItem('conductor')!);
+    console.log(this.conductor)
+
+    setTimeout( ()=>{
+      this.router.navigate(['/home'])
+    },10000)
+
   }
+
+  ionViewDidEnter(){
+    this.obtenerCoordenadas().then(() => {
+      this.map = L.map('mapId',{
+        zoomControl: false,
+      }).setView([this.latitud, this.longitud], 15);
+      L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png', {
+      }).addTo(this.map);
+      
+
+      this.startPoint = L.latLng(this.latitud, this.longitud)
+
+      this.locationMe = L.marker([this.latitud, this.longitud]).addTo(this.map);
+    });
+    
+  };
+  ionViewWillLeave(){
+    if (this.map) {
+      this.map.off();
+      this.map.remove();
+    }
+  };
+
+  
+
 
 }
