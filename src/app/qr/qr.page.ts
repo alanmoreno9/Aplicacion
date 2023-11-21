a2f3324fcaa0ad3df0f6bafa99e3f79e8c2323ff
Alex <pavezzalex@gmail.com>
2023-11-21 19:20:15 -0300

    cambios

--- src/app/qr/qr.page.ts
+++ src/app/qr/qr.page.ts
@@ -1,15 +1,12 @@
-import { Component, ViewChild, OnDestroy } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-
-
 
 @Component({
   selector: 'app-qr',
   templateUrl: './qr.page.html',
   styleUrls: ['./qr.page.scss'],
 })
-export class QrPage implements OnDestroy {
+export class QrPage implements OnInit {
   scannedResult: any;
   content_visibility='';
 
@@ -20,52 +17,5 @@ export class QrPage implements OnDestroy {
   ngOnInit() {
   }
 
-  async checkPermission(){
-    try {
-      const status = await BarcodeScanner.checkPermission({force:true});
-      if (status.granted) {
-        return true;
-      }
-      return false;
-    } catch (error) {
-      return undefined;
-    }
-  }
-
-  async startScan(){
-    try {
-      const permission = await this.checkPermission();
-      if (!permission) {
-        return;
-      }
-      await BarcodeScanner.hideBackground();
-      document.querySelector('body')?.classList.add('scanner-active');
-      this.content_visibility='hidden';
-      const result = await BarcodeScanner.startScan();
-      console.log(result);
-      BarcodeScanner.showBackground()
-      document.querySelector('body')?.classList.remove('scanner-active');
-      this.content_visibility='';
-      if (result?.hasContent) {
-        this.scannedResult = result.content;
-        console.log(this.scannedResult);
-        this.router.navigate(['/home']);
-      }
-    } catch (error) {
-      console.log(error);
-      this.stopScan();
-    }
-  }
-
-  stopScan(){
-    BarcodeScanner.showBackground();
-    BarcodeScanner.stopScan(); 
-    document.querySelector('body')?.classList.remove('scanner-active');
-    this.content_visibility='';
-  }
-
-  ngOnDestroy(): void {
-    this.stopScan();
-  }
 
 }
